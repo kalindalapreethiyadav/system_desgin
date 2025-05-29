64912cf742c78e2ab592c23815abd0d2632ab699
kalindalapreethiyadav <kpreethiyadav579@gmail.com>
2025-05-29 14:02:55 +0530

    apply terrafomr version

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -4,14 +4,14 @@
 # Terraform POc's
 ########################################
 
-"""To store the Terraform state file in a remote backend (like AWS S3), 
-you define the backend configuration in a file typically named backend.tf.
+/*To store the Terraform state file in a remote backend (like AWS S3), 
+you define the backend configuration in a file typically named backend.tf. 
 
 bucket --> Name of the S3 bucket to store the state file.
 key: Path within the bucket (acts like a file path).
 region: AWS region where the bucket is located.
 dynamodb_table: (Optional) Enables state locking to prevent concurrent changes.
-encrypt: Ensures the state file is encrypted at rest."""
+encrypt: Ensures the state file is encrypted at rest. */
 ########################################################
 
 
