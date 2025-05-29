76bff128ad00ae6edcca6d6f137fcee4a3691ea8
kalindalapreethiyadav <kpreethiyadav579@gmail.com>
2025-05-29 14:35:59 +0530

    added state file

--- terraform/aws/modules/ec2/ec2-instnace.tf
+++ terraform/aws/modules/ec2/ec2-instnace.tf
@@ -4,8 +4,8 @@
 ########################################
 
 resource "aws_instance" "main_default" {
-    instance_type = "t2-micro"
-    ami = "ami-83hf892gh894gf79"
+    instance_type = "t2.micro"
+    ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
     tags = {
         description = "ec2-default"
         owner = "preethi@gmail.com"
