9836a1545766cd98320052afca7b31a4c20e5983
kalindalapreethiyadav <kalindalapreethiyadav@gmail.com>
2026-06-11 18:44:07 +0530

    testing

--- pre-rocket/terraform/variables.tf
+++ pre-rocket/terraform/variables.tf
@@ -1,8 +1,12 @@
 
 variable "access_key" {
+    description = "This is access key"
     type = string
+    default = "bdhduohsjabckjsc"
 }
 
 variable "secret_key" {
+    description = "This is secert key"
     type = string
+    default = "bdhduohsjabckjsc"
 }
