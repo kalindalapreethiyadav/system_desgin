7bfedc1dfeead7ce67963097702c84907c66ac49
kalindalapreethiyadav <kalindalapreethiyadav@gmail.com>
2026-06-15 14:00:11 +0530

    added changes

--- pre-rocket/terraform/provider.tf
+++ pre-rocket/terraform/provider.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "us-east-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- pre-rocket/terraform/variables.tf
+++ pre-rocket/terraform/variables.tf
@@ -1,12 +1,17 @@
 
-variable "access_key" {
+variable "AWS_ACCESS_KEY_ID" {
     description = "This is access key"
     type = string
     default = "bdhduohsjabckjsc"
 }
 
-variable "secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
     description = "This is secert key"
     type = string
     default = "bdhduohsjabckjsc"
+}
+
+variable "region" {
+  type = string
+  default = "us-east-1"
 }
