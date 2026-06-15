19f78b621c7d163698e38e2ed1f6057272baa5c6
kalindalapreethiyadav <kalindalapreethiyadav@gmail.com>
2026-06-15 20:41:42 +0530

    added changes

---
+++ pre-rocket/terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "${var.Tenant}-${var.Enviornment}-bucket"
+    key            = "${var.Tenant}/${var.Enviornment}/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
