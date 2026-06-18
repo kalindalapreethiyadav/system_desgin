414118b1458ca6c454e349bb523a4ef48458337a
kalindalapreethiyadav <kalindalapreethiyadav@gmail.com>
2026-06-18 13:57:47 +0530

    adding changes

--- pre-rocket/terraform/main.tf
+++ pre-rocket/terraform/main.tf
@@ -5,4 +5,6 @@ module "aws_iam_role" {
 module "ec2_server" {
     source = "./modules/ec2"
     ec2_instance_profile_name  = var.ec2_instance_profile_name
+    depends_on = [ module.aws_iam_role ]
 }
+
